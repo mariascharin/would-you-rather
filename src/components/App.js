e5c5d1f5b7bce834b92d096b50be5753f5de84aa
Maria Scharin <maria.scharin@comvalresearch.com>
2021-01-17 09:52:55 +0100

    Get and handle initial data

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,28 @@ import HomePage from './HomePage';
 import QuestionPage from './QuestionPage';
 import NewQuestionPage from './NewQuestionPage';
 import LeaderBoardPage from './LeaderBoardPage';
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-          <div className='contains'>
-              <Nav />
-              <div>
-                  <Route path='/' exact component={HomePage} />
-                  <Route path='/questions/:id' component={QuestionPage} />
-                  <Route path='/add' component={NewQuestionPage} />
-                  <Route path='/leaderboard' component={LeaderBoardPage} />
-              </div>
-          </div>
-        </Router>
-    )
-  }
+    componentDidMount() {
+        this.props.dispatch(handleInitialData())
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className='contains'>
+                    <Nav/>
+                    <div>
+                        <Route path='/' exact component={HomePage}/>
+                        <Route path='/questions/:id' component={QuestionPage}/>
+                        <Route path='/add' component={NewQuestionPage}/>
+                        <Route path='/leaderboard' component={LeaderBoardPage}/>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App

221af6e836f6641d6e86c4677b6878d712142616
Maria Scharin <maria.scharin@comvalresearch.com>
2021-01-18 13:17:07 +0100

    List questions on home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import Login from './Login';
-import QuestionList from './QuestionList';
+import Question from "./Question";
 
 class HomePage extends Component {
-  render() {
-    return (
-      <div>
-          <Login />
-          <QuestionList />
-          HOME PAGE
-      </div>
-    )
-  }
+    render() {
+        console.log('HomePage component props: ', this.props)
+        return (
+            <div>
+                <Login />
+                <h3 className='center'>Question List</h3>
+                <ul className='dashboard-list'>
+                    {this.props.questionIds.map((id) => (
+                            <li key={id}>
+                                <div>QUESTION ID: {id}</div>
+                            </li>
+                        )
+                    )}
+                </ul>
+                <Question />
+            </div>
+        )
+    }
 }
 
-export default HomePage
+function mapStateToProps({ questions }) {
+    return {
+        questionIds: Object.keys(questions)
+            .sort((a, b) =>
+            questions[b].timeStamp - questions[a].timeStamp)
+    }
+}
+
+export default connect(mapStateToProps)(HomePage)

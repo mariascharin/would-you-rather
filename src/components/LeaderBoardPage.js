d42baf157bc4bcdca58407c4b144aa7c85832643
Maria Scharin <maria.scharin@comvalresearch.com>
2021-01-31 09:34:33 +0100

    Leader Board

--- src/components/LeaderBoardPage.js
+++ src/components/LeaderBoardPage.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import UserDetails from './UserDetails';
+import QuestionSummary from "./QuestionSummary";
+import {Tab} from "react-bootstrap";
 
 class LeaderBoardPage extends Component {
   render() {
     return (
-      <div>
-        <UserDetails />
-        LEADER BOARD PAGE
-      </div>
+        <div>
+            <h3 className='center'>Leader Board</h3>
+            <ul className='dashboard-list'>
+                {this.props.usersIds
+                    .map((id) => (
+                            <li key={id}>
+                                <UserDetails id={id} />
+                            </li>
+                        )
+                    )}
+            </ul>
+        </div>
     )
   }
 }
 
-export default LeaderBoardPage
+function mapStateToProps({users, questions}) {
+    return {
+        usersIds: Object.keys(users),
+        users,
+        questions,
+    }
+}
+
+export default connect(mapStateToProps)(LeaderBoardPage)
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
+import {Badge, ProgressBar} from "react-bootstrap";
 
 class UserDetails extends Component {
   render() {
+    const { userId, users, questions } = this.props;
+    const user = users[userId];
+    const questionsAsked = Object.values(questions)
+        .filter((question)=>(question.author === userId))
+        .length;
+    const questionsAnswered = Object.values(questions)
+        .filter((question)=>(
+            question.optionOne.votes.includes(userId) ||
+            question.optionTwo.votes.includes(userId)
+        ))
+        .length;
+    const totalScore = questionsAsked + questionsAnswered;
+    const userStats = {
+      userId,
+      questionsAsked,
+      questionsAnswered,
+      totalScore,
+    }
+
     return (
-      <div>
-      </div>
+        <div>
+          <div className='tweet'>
+            <img
+                src={user.avatarURL}
+                alt={`Avatar of ${user.name}`}
+                className='avatar'
+            />
+            <div className='tweet-info'>
+              <div>
+                <span>{user.name}</span>
+                <div className='question-detail'>
+                  <span>Answered questions: <b>{userStats.questionsAnswered}</b></span>
+                </div>
+                <div className='question-detail'>
+                  <span>Asked questions: <b>{userStats.questionsAsked}</b></span>
+                </div>
+              </div>
+            </div>
+            <div className='question-detail'>
+              <span>Score: <b>{userStats.totalScore}</b></span>
+            </div>
+          </div>
+        </div>
     )
   }
 }
 
-export default UserDetails
+function mapStateToProps({users, questions}, props) {
+  const { id }=props;
+  return {
+    userId: id,
+    users,
+    questions,
+  }
+}
+
+export default connect(mapStateToProps)(UserDetails)
